19677430e3b6048403d519351e916ff078b3a0b1
Dustin Hodges <dustin@volantesoftware.com>
2022-03-28 00:27:52 -0600

    removing auto-gen'd component and service

--- projects/volante/slottrak-ui/src/lib/slottrak-ui.module.ts
+++ projects/volante/slottrak-ui/src/lib/slottrak-ui.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
-import { SlottrakUiComponent } from './slottrak-ui.component';
-
-
 
 @NgModule({
   declarations: [
-    SlottrakUiComponent
   ],
   imports: [
   ],
   exports: [
-    SlottrakUiComponent
   ]
 })
 export class SlottrakUiModule { }
--- projects/volante/slottrak-ui/src/public-api.ts
+++ projects/volante/slottrak-ui/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of slottrak-ui
  */
 
-export * from './lib/slottrak-ui.service';
-export * from './lib/slottrak-ui.component';
 export * from './lib/slottrak-ui.module';
